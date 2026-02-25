df1f982b3384cfa9efd656ab8af9418c01c1b059
vilayat571 <vilayat571@gmail.com>
2026-02-25 18:35:26 +0800

    fix: debug the image

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://notevaultapi-production.up.railway.app/';
+//const BASE_URL = 'https://notevaultapi-production.up.railway.app/';
+const BASE_URL = 'http://localhost:3000';
 
 const api = axios.create({ baseURL: BASE_URL });
 
